96f466f060a69783f1a42779e46bded3469dae37
Rifatovich <95916047+Rifatovich@users.noreply.github.com>
2023-12-04 03:22:01 +0300

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -8,8 +8,6 @@ class TestParser(unittest.TestCase):
         self.ip_prop = ip_protocol_prop
         self.pcap = dpkt.pcap.Reader(open('temp/testsforparser.pcap','rb'))
         self.ip = "bad ip"
-    def test_parser(self):
-        self.assertEqual(self.parser(self.pcap)[0]["length"], 52)
     def test_prot_prop(self):
         self.assertEqual(self.ip_prop(self.ip), "No protocol")  
 if __name__ == "__main__":
